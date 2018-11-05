c034e75ef148074b3e90de9c5c8f030bcf426f86
Cathie Wells <c.wells18@imperial.ac.uk>
2018-11-05 19:49:10 +0000

    Fixed and with comments

--- CTCS.py
+++ CTCS.py
@@ -14,23 +14,23 @@ def CTCS(phiOld, c, nt):
     nx = len(phiOld)
 
     # new time-step array for phi as scheme requires centring in time.
+    phi = phiOld.copy()
     phi1 = phiOld.copy()
     phi2 = phiOld.copy()
-    phi3 = phiOld.copy()
+    
     # FTCS to generate first line of new data.
     for j in range(nx):
-        phi2[j] = phiOld[j] - 0.5*c*\
-        (phiOld[(j+1)%nx] - phiOld[(j-1)%nx])    
+        phi2[j] = phiOld[j] - 0.5*c*(phiOld[(j+1)%nx] - phiOld[(j-1)%nx])    
     # CTCS for each time-step after the first line has been generated
     for it in range(nt-1):
-        # Loop through all space using remainder after division (%)
-        # to cope with periodic boundary conditions
+        # Loop through all space using mod nx to ensure periodic
+        # boundary conditions
         for j in range(nx):
-            phi3[j] = phi1[j] - c*(phi2[(j+1)%nx] - phi2[(j-1)%nx])
+            phi[j] = phi1[j] - c*(phi2[(j+1)%nx] - phi2[(j-1)%nx])
             # update arrays for next time-step
-            phi1=phi2.copy()
-            phi2=phi3.copy()
+        phi1=phi2.copy()
+        phi2=phi.copy()
      
+    return phi
 
-    return phi2
 
