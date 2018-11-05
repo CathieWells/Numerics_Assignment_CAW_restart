81d8ea9ee5f0e07e979df212025423b748d8de9f
Cathie Wells <c.wells18@imperial.ac.uk>
2018-11-05 20:27:10 +0000

    Updated to include specific file names.

--- frameFTBS.py
+++ frameFTBS.py
@@ -8,31 +8,36 @@ Created on Sat Nov  3 13:14:49 2018
 
 import matplotlib.pyplot as plt
 
-# read in all the linear advection schemes, initial conditions and other
+# Read in relevant linear advection scheme, initial conditions and other
 # code associated with this application
 from Initial_conditions import *
-from LW import *
 from FTBS import *
-from CTCS import *
 from Error_checks import *
 
-### The main code is inside a function to avoid global variables    ###
+#This code will allow generation of FTBS results for each of the
+#initial conditions, where 1 is cos bell, 2 is square wave and 3 is mixed.
+#The resolution can also be changed to allow for comparison, with number
+#of time steps for both space (nx) and time (nt) required in function call.
 def FTBS_graph(nx,nt,condition):
     "Advect different initial conditions using FTBS scheme"
     "feed in resolution in both time and space"
     "and preferred initial conditions"
 
-    # Parameters maintained throughout.
+    # Constants assigned for all calculations.
     xmin = 0
     xmax = 1
     c = 0.2
-    # Derived parameters
+    
+    # Calculated value from constants and user specified nx value.
     dx = (xmax - xmin)/nx
    
-    # spatial points for plotting and for defining initial conditions
+    # Spatial points for plotting and for defining initial conditions
     x = np.arange(xmin, xmax, dx)
     
-    #Initial Conditions and shifted conditions.
+    #Initial Conditions and shifted conditions, using if to allow
+    #user to specify, via third function call parameter, 
+    #which initial wave to use.
+    #System exit included where initial conditions not called appropriately.
     if condition==1:
         phiOld = cosBell(x, 0, 0.75)
         phiAnalytic= cosBell((x - c*nt*dx)%(xmax - xmin), 0, 0.75)
@@ -46,11 +51,12 @@ def FTBS_graph(nx,nt,condition):
         print("I do not understand your chosen initial conditions.")
         raise SystemExit
     
-    # Advect the profile using finite difference for all the time steps
+    # Call in numerical advection code from function FTBS.
     phiFTBS= FTBS(phiOld.copy(), c, nt)
     
 
-    # Plot the solutions
+    # Plot inital conditions, analytic result and numerical model for
+    #for given resolution.
     font = {'size'   : 20}
     plt.rc('font', **font)
     plt.figure(1)
@@ -64,8 +70,10 @@ def FTBS_graph(nx,nt,condition):
     plt.ylim([-0.2,1.2])
     plt.legend(bbox_to_anchor=(1.15 , 1.1))
     plt.xlabel('$x$')
+    plt.ylabel('$phi(x)$')
+    #Save each graph to the graphs_tables folder to be used in report.
+    #Allow graph to save into graphs_tables folder.
+    #File name reflects resolution to avoid overwriting.
     input('press return to save file and continue')
-    plt.savefig('graphs_tables/FTBS3.pdf')
-
-### Run the function main defined in this file                      ###
-FTBS_graph(40,40,2)
+    plt.savefig('graphs_tables/FTBS_analysis_%d_%d_%d.pdf'%(nx,nt,condition))
+    
