e0693e223aa534b001d9891679bd54c915d46569
Cathie Wells <c.wells18@imperial.ac.uk>
2018-11-19 11:50:28 +0000

    Adjusted to cover just square and bell.

--- Time_time_steps.py
+++ Time_time_steps.py
@@ -7,7 +7,7 @@ Created on Tue Nov  6 08:23:58 2018
 """
 
 
-    
+#Read in extra code.   
 import matplotlib.pyplot as plt
 import numpy as np
 import time
@@ -17,42 +17,55 @@ from Initial_conditions import *
 from LW import *
 from FTBS import *
 from CTCS import *
-from datetime import datetime
-
 
 
 #Call functions to solve each scheme to a certain number of time steps
 #for fixed spatial resolution. 
 #This means advection speed will vary as courant number is fixed.
-def comp_time_cost(PhiOld,type):
+def comp_time_cost(type):
     "Advect the initial conditions fed in using various advection"
-    "schemes for given c and compare results"
+    "schemes for given c and time process"
 
-    # Fixed parameters throughout for all three schemes.
-    Xmin = 0
-    Xmax = 1
+# Fixed parameters throughout for all three schemes.
+    xmin = 0
+    xmax = 1
     nx=100
     c=0.5
     
     
-    # Derived parameters for 
-    dx = (Xmax - Xmin)/nx
+# Derived parameters for 
+    dx = (xmax - xmin)/nx
     
-    # Spatial points for plotting and for defining initial conditions
-    X = np.arange(Xmin, Xmax, dx)
-    
-    #Set up array to hold timing for each calculation.
-    cost=np.zeros((3,10))
+# Spatial points for plotting and for defining initial conditions
+    x = np.arange(xmin, xmax, dx)
+  
+#Set up array to hold timing for each calculation.
+    cost=np.zeros((6,10))
     a = np.zeros(100)
-    #Start loop to alter timesteps.
-    #Compute end results for times at 10 step intervals.
+#Specifiy initial conditions to use for input name.
+    if type=='square':
+        phiOld = squareWave(x)
+    else:
+        phiOld = cosBell(x, 0, 0.75)
+#Start loop to alter timesteps.
+#Compute end results for times at 20 step intervals.
     for i in range (0,10):
         nt=(i+1)*20
-    
-        # Advect the profile using finite difference
-        #for all the time step variations for each of the three schemes
-        #Put these results into a matrix.
+#Collect time for each scheme at each time step once.
+        start = time.time()
+        FTBS(phiOld.copy(), c, nt)
+        cost[3][i]=float(time.time()-start)
+        start = time.time()
+        CTCS(phiOld.copy(), c, nt)
+        cost[4][i]=float(time.time()-start)
+        start = time.time()
+        LW(phiOld.copy(), c, nt)
+        cost[5][i]=float(time.time()-start)
         
+# Advect the profile using finite difference/volume
+#for all the time step variations for each of the three schemes
+#Put timings for each time step into an array.
+#Put minimum time from each array into a 2D array.
         
         for j in range (0,100):
             start = time.time()
@@ -66,41 +79,63 @@ def comp_time_cost(PhiOld,type):
         cost[1][i]=a.min()
         for l in range (0,100):
             start = time.time()
-            FTBS(phiOld.copy(), c, nt)
+            LW(phiOld.copy(), c, nt)
             a[l]=float(time.time()-start)
         cost[2][i]=a.min()
-        
-    
-    #Define x for graph axes
-    x=np.arange(20,220,20)
-    
-    
-    #Take each line of the matrix out as an array for plotting.
-    FTBS_cost=cost[0]
-    CTCS_cost=cost[1]
-    LW_cost=cost[2]
+ 
+#Define x for graph axes
+    X=np.arange(20,220,20)
     
     
-    # Plot the solutions for all schemes on one set of axes.
+# Plot the solutions for all schemes on one set of axes for single timing.
     font = {'size'   : 20}
     plt.rc('font', **font)
     plt.figure(1)
     plt.clf()
     plt.ion()
-    plt.plot(x,FTBS_cost , label='FTBS', color='blue')
-    plt.plot(x, CTCS_cost, label='CTCS', color='red')
-    plt.plot(x, LW_cost, label='LW', color='green')
+    plt.plot(X,cost[3] , label='FTBS', color='blue',\
+             linestyle='--', marker='s')
+    plt.plot(X, cost[4], label='CTCS', color='green',\
+             linestyle='--', marker='^')
+    plt.plot(X, cost[5], label='LW', color='pink',\
+             linestyle='--', marker='o')
     plt.axhline(0, linestyle=':', color='black')
-    plt.ylim([-0.001,0.012])
+    plt.ylim([-0.001,0.03])
     plt.legend(bbox_to_anchor=(1.15 , 1.1))
     plt.xlabel('No. of time steps')
     plt.ylabel('Computational time(s)')
-    #Allow graph to save into graphs_tables folder.
+#Allow graph to save into figures folder.
     input('press return to save file and continue')
-    plt.savefig('graphs_tables/3scheme_time_time_analysis_%s.pdf'%(type), 
+    plt.savefig('figures/3scheme_single_time_analysis_%s.pdf'%(type), 
     bbox_inches = "tight")
     
-comp_time_cost(mixed(x,0,0.1,0.2,0.4),'mixed')
+    
+# Plot the solutions for all schemes on one set of axes for min over 100.
+    font = {'size'   : 20}
+    plt.rc('font', **font)
+    plt.figure(1)
+    plt.clf()
+    plt.ion()
+    plt.plot(X,cost[0] , label='FTBS', color='blue',\
+             linestyle='--', marker='s')
+    plt.plot(X, cost[1], label='CTCS', color='green',\
+             linestyle='--', marker='^')
+    plt.plot(X, cost[2], label='LW', color='pink',\
+             linestyle='--', marker='o')
+    plt.axhline(0, linestyle=':', color='black')
+    plt.ylim([-0.001,0.03])
+    plt.legend(bbox_to_anchor=(1.15 , 1.1))
+    plt.xlabel('No. of time steps')
+    plt.ylabel('Computational time(s)')
+#Allow graph to save into figures folder.
+    input('press return to save file and continue')
+    plt.savefig('figures/3scheme_100min_time_analysis_%s.pdf'%(type), 
+    bbox_inches = "tight")
+    return()
+    
+comp_time_cost('cos')
+comp_time_cost('square')
+    
 
 
     
