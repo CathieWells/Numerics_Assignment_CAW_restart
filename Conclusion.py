7a2b03adf4b4e05f3ee552308e162ece6761dd61
Cathie Wells <c.wells18@imperial.ac.uk>
2018-11-19 10:12:35 +0000

    Realigned comments

--- Conclusion.py
+++ Conclusion.py
@@ -24,29 +24,29 @@ def con_square(nx,nt,c):
     "Advect the initial square wave conditions using various advection schemes"
     "and compare results"
 
-    # Fixed parameters throughout for all three schemes.
+# Fixed parameters throughout for all three schemes.
     xmin = 0
     xmax = 1
    
     
-    # Derived parameters
+# Derived parameters
     dx = (xmax - xmin)/nx
     
-    # Spatial points for plotting and for defining initial conditions
+# Spatial points for plotting and for defining initial conditions
     x = np.arange(xmin, xmax, dx)
 
-    # Initial conditions
+# Initial conditions
     phiOld = squareWave(x)
-    # Exact solution is the initial condition advected.
+# Exact solution is the initial condition advected.
     phiAnalytic = squareWave((x - c*nt*dx)%(xmax - xmin))
 
-    # Advect the profile using finite difference
-    #for all the time steps for each of the three schemes
+# Advect the profile using finite difference
+#for all the time steps for each of the three schemes
     phiLW = LW(phiOld.copy(), c, nt)
     phiWB = WB(phiOld.copy(), c, nt)
     phiCombi = Combi(phiOld.copy(),c,nt)
     
-    # Calculate and structure error norms in a table.
+# Calculate and structure error norms in a table.
     Scheme=["LW","WB","Combi"]
     l2_error=[l2ErrorNorm(phiLW, phiAnalytic),\
     l2ErrorNorm(phiWB, phiAnalytic),l2ErrorNorm(phiCombi, phiAnalytic)]
@@ -54,9 +54,9 @@ def con_square(nx,nt,c):
     lInfErrorNorm(phiWB, phiAnalytic),lInfErrorNorm(phiCombi, phiAnalytic)]
     table=zip(Scheme,l2_error,linf_error)
     
-    #Set up array for min_max data.
+#Set up array for min_max data.
     min_max=np.zeros((7,nt))
-    #Check max and min at each timestep for each scheme:
+#Check max and min at each timestep for each scheme:
     for i in range (nt+1):
         min_max[0][i-1]=np.min(LW(phiOld.copy(), c, i))
         min_max[1][i-1]=np.max(LW(phiOld.copy(), c, i))
@@ -66,7 +66,7 @@ def con_square(nx,nt,c):
         min_max[5][i-1]=np.max(Combi(phiOld.copy(), c, i))
         min_max[6][i-1]=i
         
-    #Send table to figures folder. 
+#Send table to figures folder. 
     file=open("figures/Conclusion_Square_errors_%d_%d.txt"%(nx,nt),"w")
     file.write(tabulate(table, headers=\
     ["Scheme", "l2 error norm","linf error norm"]\
@@ -74,7 +74,7 @@ def con_square(nx,nt,c):
     file.close()
     
     
-    # Plot the solutions for all schemes on one set of axes.
+# Plot the solutions for all schemes on one set of axes.
     font = {'size'   : 20}
     plt.rc('font', **font)
     plt.figure(1)
@@ -91,13 +91,13 @@ def con_square(nx,nt,c):
     plt.legend(bbox_to_anchor=(1.15 , 1.1))
     plt.xlabel('$x$')
     plt.ylabel('phi(x)')
-    #Allow graph to save into graphs_tables folder.
-    #File name reflects resolution to avoid overwriting.
+#Allow graph to save into graphs_tables folder.
+#File name reflects resolution to avoid overwriting.
     input('press return to save file and continue')
     plt.savefig('figures/conclusion_square_analysis_%d_%d.pdf'%(nx,nt), 
     bbox_inches = "tight")
     
-    #Plot bounds at each timestep for each scheme.
+#Plot bounds at each timestep for each scheme.
     x=min_max[6]
     font = {'size'   : 20}
     plt.rc('font', **font)
@@ -124,8 +124,8 @@ def con_square(nx,nt,c):
     plt.legend(bbox_to_anchor=(1.15 , 1.1))
     plt.xlabel('no of time steps')
     plt.ylabel('phi(x)')
-    #Allow graph to save into figures folder.
-    #File name reflects resolution to avoid overwriting.
+#Allow graph to save into figures folder.
+#File name reflects resolution to avoid overwriting.
     input('press return to save file and continue')
     plt.savefig('figures/conclusion_min_max_square_analysis_%d.pdf'%(nx), 
     bbox_inches = "tight")
@@ -133,6 +133,7 @@ def con_square(nx,nt,c):
 
     return()
 con_square(40,80,0.5)
+
 
 
 
