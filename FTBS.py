d26bcbe726836fe637afd0342c9e07f2642f301f
Cathie Wells <c.wells18@imperial.ac.uk>
2018-11-19 11:13:46 +0000

    Aligned comments

--- FTBS.py
+++ FTBS.py
@@ -4,29 +4,28 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+#Open necessary code.
 import numpy as np
 #Create Forwards in Time Backwards in Space code.
 def FTBS(phiOld, c, nt):
     "Linear advection of profile in phiOld using FTBS, Courant number c"
     "for nt time-steps"
-    #Set number of space time steps to follow length of initial data given.
+#Set number of space time steps to follow length of initial data given.
     nx = len(phiOld)
 
-    #Create an array to store the current copy of phi values at x gridpoints.
+#Create an array to store the current copy of phi values at x gridpoints.
     phi = phiOld.copy()
 
-    # For each of the nt time steps, go through the loop.
+# For each of the nt time steps, go through the loop.
     for it in range(nt):
-        #Calculate new time step phi based on previous time step phi in
-        #current and previous positions.
-        # Use mod nx on spatial position to allow looping of values, 
-        #thus giving periodic boundary conditions.
+#Calculate new time step phi based on previous time step phi in
+#current and previous positions.
+# Use mod nx on spatial position to allow looping of values, 
+#thus giving periodic boundary conditions.
         for j in range(nx):
-            phi[j] = phiOld[j] - c*\
-                     (phiOld[(j)%nx] - phiOld[(j-1)%nx])
-        #Move current results into phiOld array, to be referred to for next
-        #timestep.
+            phi[j] = phiOld[j] - c*(phiOld[(j)%nx] - phiOld[(j-1)%nx])
+#Move current results into phiOld array, to be referred to for next
+#timestep.
         phiOld = phi.copy()
-    #Output phi at final timestep for all x gridpoints.
+#Output phi at final timestep for all x gridpoints.
     return phi
